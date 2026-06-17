5227d4d5971a7350c02c37fba75ab30009cfc844
Stephen Macabulos <myteseeker18@gmail.com>
2026-06-17 20:31:27 +0800

    feat: direnv

--- flake.nix
+++ flake.nix
@@ -1,45 +1,19 @@
 {
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    # Add Home Manager
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  description = "Kubectl, Terraform, and ArgoCD";
 
-  };
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs, home-manager, ... }:
+  outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
-      sharedModules = [
-        home-manager.nixosModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.seeker = import ./home.nix;
-        }
-
-        {
-          nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (nixpkgs.lib.getName pkg) [
-            "obsidian"
-          ];
-        }
-      ];
+      pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
     in
     {
-      nixosConfigurations = {
-        desktop = nixpkgs.lib.nixosSystem {
-          inherit system;
-          modules = [
-            ./hosts/desktop/configuration.nix
-          ] ++ sharedModules;
-        };
-
-        laptop = nixpkgs.lib.nixosSystem {
-          inherit system;
-          modules = [
-            ./hosts/laptop/configuration.nix
-          ] ++ sharedModules;
-        };
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          kubectl
+          argocd
+        ];
       };
     };
 }
