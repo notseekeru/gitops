706bffac6b3179e4bb9e87cd0863fdc1c1f506c9
Stephen Macabulos <myteseeker18@gmail.com>
2026-09-17 20:22:56 +0800

    chore(flake): updated flake and lock

--- flake.nix
+++ flake.nix
@@ -1,20 +1,26 @@
 {
-  description = "Kubectl, Terraform, and ArgoCD";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs-infisical.url = "https://channels.nixos.org/nixos-unstable/nixexprs.tar.xz";
+  };
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, nixpkgs-infisical }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
+      infisical = nixpkgs-infisical.legacyPackages.${system}.infisical;
     in
     {
       devShells.${system}.default = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          kubectl
-          argocd
+        buildInputs = [
           infisical
-        ];
+        ] ++ (with pkgs; [
+          kubectl
+          terraform
+          argocd
+          aws-nuke
+        ]);
       };
     };
 }
+
